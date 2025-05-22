9b2a73944884ce579cf21091a82443e7f8d66a04
zogodo <7427829008@qq.com>
2025-05-22 09:33:45 +0800

    add my_run.sh

---
+++ src/ch2/section2.2/2.2.1/my_run.sh
@@ -0,0 +1,4 @@
+
+export WORK_HOME=/store_121/zgd/puvm/
+export UVM_HOME=/store_121/EDA/INCISIV131S013/tools.lnx86/uvm-1.1/uvm_lib/uvm_sv
+irun -gui -access rwc -64bit -uvm +sv -f filelist.f -licqueue -timescale 1ns/1ps
